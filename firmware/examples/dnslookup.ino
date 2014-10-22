fb9528945d617a9ade7863bebab922d66c85bdbb
Hootie81 <chris.huitema@iinet.net.au>
2014-10-22 19:19:17 +0800

    Extra comments in Example
    Added extra comments

--- firmware/examples/dnslookup.ino
+++ firmware/examples/dnslookup.ino
@@ -1,27 +1,26 @@
-// This #include statement was automatically added by the Spark IDE.
 
 
-#include "DNSClient.h"
+#include "DNSClient.h"  //include the Library
 
 
-IPAddress dnsServerIP(8,8,8,8);
-char serverName[] = "api.pushingbox.com";
-IPAddress remote_addr;
-DNSClient dns;
+IPAddress dnsServerIP(8,8,8,8);  			//Set the DNS server to use for the lookup
+char serverName[] = "api.pushingbox.com";  	//This is the server we are looking for the IP of
+IPAddress remote_addr;   					//This is where we save the IP Address
+DNSClient dns;  							//This sets up an instance of the DNSClient
 
 
 
 void setup() {
-    Serial.begin(9600);
-    while (!Serial.available()) SPARK_WLAN_Loop();
+    Serial.begin(9600);						//Turn on the Serial port so we can see the example
+    while (!Serial.available()) SPARK_WLAN_Loop();  //Wait for user to press key (for windows users)
     while (Serial.available()) Serial.read(); //throw it away
     
     Serial.print("Host to lookup: ");
     Serial.println(serverName);
     
     int ret = 0;
-    dns.begin(dnsServerIP);
-    ret = dns.getHostByName(serverName, remote_addr);
+    dns.begin(dnsServerIP);					//this send the DNS server to the library
+    ret = dns.getHostByName(serverName, remote_addr);	//ret is the error code, getHostByName needs 2 args, the server and somewhere to save the Address
     if (ret == 1) {
         Serial.print("Host to lookup: ");
         Serial.println(serverName);
@@ -31,7 +30,13 @@ void setup() {
         Serial.println("Error code: ");
         Serial.println(ret);
     }
-    
+	
+//    Error Codes
+//SUCCESS          1
+//TIMED_OUT        -1
+//INVALID_SERVER   -2
+//TRUNCATED        -3
+//INVALID_RESPONSE -4
 
 }
 
